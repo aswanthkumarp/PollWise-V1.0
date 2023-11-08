c32cebf603fbc151f842fc9cf9c8f20921cfb6e8
Aswanth Kumar P <aswanthkumar777@gmail.com>
2023-11-08 23:02:04 +0530

    socket error

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,6 +1,6 @@
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client';
 
-const URL = "https://pollwise.netlify.app";
+const URL = 'https://pollwisev1.onrender.com';
 
 export const socket = io(URL, {
   autoConnect: false,
